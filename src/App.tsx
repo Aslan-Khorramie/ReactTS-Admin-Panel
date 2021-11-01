6a09d6e1ea7a910bb491c7f1b4ae71e51f5ade05
Aslan Khorramie <aslan.kh.dostan@gmail.com>
2021-11-01 18:57:15 +0330

    test webstorm config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import {BrowserRouter,Route, Switch} from "react-router-dom"
+
+// import pages
+import Login from './pages/Login/Login';
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={Login} />
+            <Route component={NotFound} />
+        </Switch>
+    </BrowserRouter>
   );
 }
 
